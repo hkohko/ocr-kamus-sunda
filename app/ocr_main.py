8acaeeaee2c0ea54c297486cb0f8d3f6827364fd
heko <91366057+hkohko@users.noreply.github.com>
2023-08-24 15:31:06 +0700

    regex to search by file number, minor tweaks

--- app/ocr_main.py
+++ app/ocr_main.py
@@ -1,37 +1,45 @@
 import pytesseract
 import cv2 as cv
-from io import BytesIO
+import re
 from os import listdir
 from pathlib import Path
-from PIL import Image, ImageEnhance
+from PIL import Image
 
 main_dir = Path(__file__).parent.parent
 current_dir = Path(__file__).parent
 images_dir = f"{main_dir}/images"
-subfolder = "Kamus_sunda"
+
 tesseract_location = (
     r"C:\Users\rifdi\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 )
 pytesseract.pytesseract.tesseract_cmd = tesseract_location
-page = 173
 
 
 def get_sample_image():
-    image_sample = listdir(f"{images_dir}/Kamus_sunda")
-    return f"{images_dir}/{subfolder}/{image_sample[page + 1]}"
+    subfolder = "Kamus_sunda"
+    page = 68
+
+    if len(str(page)) == 2:
+        page = f"0{page}"
+    elif len(str(page)) == 1:
+        page = f"00{page}"
+
+    image_sample = listdir(f"{images_dir}/{subfolder}")
+    pattern = r"-(\d+)\.\w+"
+    for img_text in image_sample:
+        match = re.search(pattern, img_text, re.IGNORECASE)
+        if match.group(1) == page:
+            return f"{images_dir}/{subfolder}/{img_text}"
 
 
-def adapt_thresh():
+def processed_img():
     img = cv.imread(get_sample_image())
-    # img = cv.medianBlur(img, 5)
-    # _,thresh1 = cv.threshold(img,thresh=245, maxval=255, type=cv.THRESH_BINARY)
-    # th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
     img = cv.fastNlMeansDenoising(img, h=5)
     image = Image.fromarray(img, "RGB")
     return image
 
 
 if __name__ == "__main__":
-    im = adapt_thresh()
+    im = processed_img()
     print(pytesseract.image_to_string(im))
     im.show()
--- app/pdf_to_image.py
+++ app/pdf_to_image.py
@@ -7,19 +7,20 @@ import logging
 logging.basicConfig(format="%(asctime)s %(message)s")
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-main_dir = Path(__file__).parent.parent
-current_dir = Path(__file__).parent
-images_dir = f"{main_dir}/images/"
+
+MAIN_DIR = Path(__file__).parent.parent
+CURRENT_DIR = Path(__file__).parent
+IMAGES_DIR = f"{MAIN_DIR}/images/"
 
 
 def create_images_dir():
-    if "images" not in listdir(main_dir):
-        mkdir(images_dir)
+    if "images" not in listdir(MAIN_DIR):
+        mkdir(IMAGES_DIR)
 
 
 def get_pdf_file(filename: str):
     pdf_dir = [
-        file for file in listdir(f"{main_dir}/pdf_files/") if file[-4:] == ".pdf"
+        file for file in listdir(f"{MAIN_DIR}/pdf_files/") if file[-4:] == ".pdf"
     ]
     for pdf in pdf_dir:
         if filename in pdf.lower():
@@ -31,7 +32,7 @@ def page_size(filename: str):
     if pdf_name is None:
         logger.warning("page_size: no pdf found with that name")
         raise ValueError
-    file = f"{main_dir}/pdf_files/{pdf_name}"
+    file = f"{MAIN_DIR}/pdf_files/{pdf_name}"
     reader = PdfReader(file)
     box = reader.pages[0].mediabox
     width = box.width
@@ -41,13 +42,13 @@ def page_size(filename: str):
 
 
 def create_images_subfolder(filename: str):
-    if filename not in listdir(images_dir):
-        mkdir(f"{images_dir}/{filename}")
+    if filename not in listdir(IMAGES_DIR):
+        mkdir(f"{IMAGES_DIR}/{filename}")
         logger.info(f"created subfolder: {filename}")
 
 
 def get_poppler_path():
-    poppler_folder = next(folder for folder in listdir(main_dir) if "poppler" in folder)
+    poppler_folder = next(folder for folder in listdir(MAIN_DIR) if "poppler" in folder)
     if poppler_folder is None:
         logger.warning("get_poppler_path: poppler path not found")
         raise ValueError
@@ -60,10 +61,10 @@ def convert_to_image(filename: str):
     pdf_file = get_pdf_file(filename)
     folder_name = pdf_file[:-4]
     create_images_subfolder(folder_name)
-    pdf_path = f"{main_dir}/pdf_files/{pdf_file}"
-    poppler_path = rf"{main_dir}/{get_poppler_path()}/Library/bin/"
-    output_folder = f"{main_dir}/images/{folder_name.lower()}/"
-    if pdf_file in listdir(images_dir):
+    pdf_path = f"{MAIN_DIR}/pdf_files/{pdf_file}"
+    poppler_path = rf"{MAIN_DIR}/{get_poppler_path()}/Library/bin/"
+    output_folder = f"{MAIN_DIR}/images/{folder_name.lower()}/"
+    if pdf_file in listdir(IMAGES_DIR):
         logger.info("subfolder already created")
     logger.info("converting to images...")
     convert_from_path(
